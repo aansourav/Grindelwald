bf0a0e71ea8bf17773b2db120f4521918c1e6443
SoUrAv <aansourav@gmail.com>
2024-07-08 21:14:06 +0600

    Added useful comments and Live site link

--- Homepage/scripts/index.js
+++ Homepage/scripts/index.js
@@ -1,37 +1,30 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: 3,
-  spaceBetween: 20,
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+    pagination: {
+        el: ".swiper-pagination",
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
 });
 
+window.addEventListener("scroll", () => {
+    var navbar = document.querySelector(".nav-header");
+    var navbarHeight = navbar.offsetHeight;
 
-// navigation
-
-window.addEventListener('scroll',()=>{
-  var navbar = document.querySelector(".nav-header");
-  var navbarHeight = navbar.offsetHeight;
-
-  if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar-scrolled");
-  } else {
-    navbar.classList.remove("navbar-scrolled");
-  }
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add("navbar-scrolled");
+    } else {
+        navbar.classList.remove("navbar-scrolled");
+    }
 });
--- Readme.md
+++ Readme.md
@@ -3,3 +3,25 @@
 ## Homepage - https://staticmania-homepage-aansourav.netlify.app
 
 ## Dashboard - https://staticmania-dashboard-aansourav.netlify.app
+
+### Description
+
+This is a task for Jr. Front-End Developer position at StaticMania.
+I've made two static web pages **_Homepage_** and **_Dashboard_**.
+
+<br/>
+
+**Homepage**
+
+1. Made with vanilla HTML, CSS & JavaScript
+2. Used FontAwesome icons
+3. Used Slick-Carousel for implementing interactive sliding on Testimonial
+4. Added hover animation and transition effects for awesome user experience
+
+<br/>
+
+**Dashboard**
+
+1. Made with HTML & TailwindCSS
+2. Customized TainwindCSS config for custom color and font
+3. Added hover animation and transition effects for awesome user experience
